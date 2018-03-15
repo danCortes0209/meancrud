13c530e325424c0b90f4ccaace27ecbb0d819205
Daniel <flkn059@gmail.com>
2018-03-15 17:45:27 -0600

    express initialized, editor changed to ecmascript 6 preset

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Mean App");
+//Call express from node_modules and use a constant to call it as a constructor
+const express = require("express");
+const app = express();
+
+//app settings
+app.set("port",process.env.PORT || 3000);
+
+
+
+//initialize app on port 3000
+app.listen(app.get("port"), () => {
+   console.log("server on port 3000")
+});
