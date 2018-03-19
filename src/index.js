9aaba5e8e1ac386c6040760b203b5b992e8bf1f1
Daniel <flkn059@gmail.com>
2018-03-18 23:55:29 -0600

    add a route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 //Call express from node_modules and use a constant to call it as a constructor
+const cors = require("cors");
 const express = require("express");
 const app = express();
+const index = require("./routes/index.js");
 
 //app settings, get the port of the machine to run
 app.set("port", process.env.PORT || 3000);
-//call the render file property from ejs
 app.engine("html",require("ejs").renderFile);
-//set ejs as the template engine
 app.set("view engine","ejs");
 
+//Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
+//routes
+app.use(index);
 
 //initialize app on port 3000
 app.listen(app.get("port"), () => {
    console.log("server on port 3000")
 });
+
