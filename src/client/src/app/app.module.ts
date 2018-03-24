22c480890c4db07723f0d0c7119a78221b413453
Daniel <flkn059@gmail.com>
2018-03-23 23:03:44 -0600

    added functions 4 the client side

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskService } from './services/task.service';
 
 
 @NgModule({
@@ -14,7 +15,7 @@ import { TasksComponent } from './components/tasks/tasks.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/client/src/app/services/task.service.ts
+++ src/client/src/app/services/task.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import "rxjs";
+
+import { Task } from '../Task';
 
 @Injectable()
 export class TaskService {
+  domain: string = "http://localhost:3000/";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getTasks(){
+    return this.http.get<Task[]>(`${this.domain}api/tasks`).map(res => res);
+  }
+
+  addTask(newTask: Task){
+    return this.http.post(`${this.domain}api/tasks`,newTask).map(res => res);
+  }
+
+  deleteTask(id){
+    return this.http.delete(`${this.domain}api/tasks/${id}`).map(res => res);
+  }
+
+  updateTask(newTask: Task, id){
+    return this.http.put(`${this.domain}api/tasks/${id}`, newTask).map(res => res);
+  }
 
 }
